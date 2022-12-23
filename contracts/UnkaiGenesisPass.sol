2d4a4b97a7f3ac2b8434c144a0fef55d3b080fd7
Bruno Bernard <contact.brunobernard@gmail.com>
2022-12-23 04:31:26 +0400

    Initial commit from npx thirdweb create

---
+++ contracts/UnkaiGenesisPass.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/base/ERC721LazyMint.sol";
+
+contract Contract is ERC721LazyMint {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _royaltyRecipient,
+        uint128 _royaltyBps
+    ) ERC721LazyMint(_name, _symbol, _royaltyRecipient, _royaltyBps) {}
+}
